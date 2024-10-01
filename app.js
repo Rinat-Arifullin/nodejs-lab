176abd2712df898466470610913ef1bc82dfbf68
rarifullin <arifullinrd@mos-team.ru>
2024-10-01 14:23:01 +0300

    workers and fork

--- app.js
+++ app.js
@@ -1,25 +1,63 @@
-const factorial = require("./factorial");
+const { fork } = require('child_process')
+const {Worker} = require('worker_threads')
+const {performance, PerformanceObserver} = require('perf_hooks')
 
-const compute = (arr)=>{
-    const arrey = []
-    for(let i = 0; i < 10000000; i++){
-        arrey.push(i*i)
+const performanceObserver = new PerformanceObserver((items)=>{
+    items.getEntries().forEach(entry=>console.log(entry.name, entry.duration))
+})
+
+performanceObserver.observe({entryTypes: ['measure']})
+
+const workerFunction = (array)=>{
+    try{
+        return new Promise((res, rej)=>{
+            performance.mark('workerFunction start')
+            const worker = new Worker('./worker.js', {
+                workerData: {array}
+            })
+            worker.on('message', (msg)=>{
+                res(msg)
+                performance.mark('workerFunction end')
+                performance.measure('workerFunction','workerFunction start', 'workerFunction end')
+            })
+            worker.on('error', rej)
+        })
+    }catch(error){
+        console.error(error.message)
     }
-    return arr.map(el=>factorial(el))
 }
 
-const main = () => {
-    performance.mark('start');
-    const result = [
-        compute([25, 20, 19, 48,30,50]),
-        compute([25, 20, 19, 48,30,50]),
-        compute([25, 20, 19, 48,30,50]),
-        compute([25, 20, 19, 48,30,50]),
-    ]
-    console.log(result)
-    performance.mark('end');
-   performance.measure('main', 'start', 'end')
-   console.log(performance.getEntriesByName('main').pop()) 
+const forkFunction = (array)=>{
+    try {
+        return new Promise((res, rej)=>{
+            performance.mark('forkFunction start')
+            const forkProcess = fork('./fork.js')
+            forkProcess.send({array})
+            forkProcess.on('message', (msg)=>{
+                res(msg)
+                performance.mark('forkFunction end')
+                performance.measure('forkFunction','forkFunction start', 'forkFunction end')
+            })
+            forkProcess.on('error', rej)
+        })
+    }catch(error){
+        console.error(error.message)
+    }
+
 }
 
-main();
+const main = async ()=>{
+    const workerResultPromise =  workerFunction([25, 19, 48, 30]);
+    const forkResultPromise = forkFunction([25, 19, 48, 30]);
+
+    workerResultPromise?.then(res=>{
+        console.log(res)
+    })
+
+    forkResultPromise?.then(res=>{
+        console.log(res)
+    })
+
+}
+
+main()
--- worker.js
+++ worker.js
@@ -1,13 +1,4 @@
-const factorial = require("./factorial")
-const {parentPort, workerData} = require('worker_threads')
-
-
-const compute = ({arr})=>{
-    const arrey = []
-    for(let i = 0; i < 10000000; i++){
-        arrey.push(i*i)
-    }
-    return arr.map(el=>factorial(el))
-}
+const { parentPort, workerData } = require('worker_threads');
+const { compute } = require('./factorial');
 
 parentPort?.postMessage(compute(workerData))
